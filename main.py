b772a3399b8c2b5fb4162055322381b6e1053708
sergey.alekseev <sergey.alekseev@osinit.com>
2024-09-01 15:19:49 +0300

    lesson 2 add endpoints

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="FastAPI_learn",
+)
+
+test_users = [
+    {"id": 1, "role": "admin", "name": "Bob"},
+    {"id": 2, "role": "investor", "name": "John"},
+    {"id": 3, "role": "trader", "name": "Matt"},
+]
 
 
-@app.get("/")
-def home():
-    return {"Hello": "World"}
+@app.get("/users/{user_id}")
+def users(user_id: int):
+    return [user for user in test_users if user.get("id") == user_id]
+
+
+fake_trades = [
+    {"id": 1, "user_id": 1, "currency": "BTC", "side": "buy", "price": 100, "amount": 3.1},
+    {"id": 2, "user_id": 1, "currency": "BTC", "side": "sell", "price": 125, "amount": 3.1},
+]
+
+
+@app.get("/trades")
+def get_trades(limit: int = 1, offset: int = 0):
+    return fake_trades[offset:][:limit]
+
+
+test_users2 = [
+    {"id": 1, "role": "admin", "name": "Bob"},
+    {"id": 2, "role": "investor", "name": "John"},
+    {"id": 3, "role": "trader", "name": "Matt"},
+]
+
+@app.post("/users/{user_id}")
+def change_user_name(user_id: int, new_name: str):
+    current_user = list(filter(lambda user: user.get("id") == user_id, test_users2))[0]
+    current_user["name"] = new_name
+    return {"status": 200, "data": current_user}

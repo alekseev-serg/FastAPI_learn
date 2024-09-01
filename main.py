2c4d05d4b503aec264ebbf65df5cd0cd578fc0bf
sergey.alekseev <sergey.alekseev@osinit.com>
2024-09-01 13:01:43 +0300

    add main app run with uvicorn

--- main.py
+++ main.py
@@ -1,29 +1,8 @@
-from typing import Annotated
-
 from fastapi import FastAPI
-from fastapi.params import Depends
-from pydantic import BaseModel
 
 app = FastAPI()
 
 
-class STaskAdd(BaseModel):
-    name: str
-    description: str | None = None
-
-
-class STask(STaskAdd):
-    id: int
-
-
-tasks = []
-
-
-@app.post("/tasks")
-async def add_task(
-        task: Annotated[STaskAdd, Depends()],
-):
-    tasks.append(task)
-    return {"ok": True}
-
-
+@app.get("/")
+def home():
+    return {"Hello": "World"}
